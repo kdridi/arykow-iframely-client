edbd7ac079da2a0799b64c946b88173d4aba10fd
Karim DRIDI <karim.dridi@gmail.com>
2013-12-09 09:34:20 +0100

    implementing the client

--- test/arykow-iframely-client_test.js
+++ test/arykow-iframely-client_test.js
@@ -3,34 +3,36 @@
 var arykow_iframely_client = require('../lib/arykow-iframely-client.js');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+	======== A Handy Little Nodeunit Reference ========
+	https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+	Test methods:
+		test.expect(numAssertions)
+		test.done()
+	Test assertions:
+		test.ok(value, [message])
+		test.equal(actual, expected, [message])
+		test.notEqual(actual, expected, [message])
+		test.deepEqual(actual, expected, [message])
+		test.notDeepEqual(actual, expected, [message])
+		test.strictEqual(actual, expected, [message])
+		test.notStrictEqual(actual, expected, [message])
+		test.throws(block, [error], [message])
+		test.doesNotThrow(block, [error], [message])
+		test.ifError(value)
 */
 
 exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(arykow_iframely_client.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
+	setUp: function(done) {
+		// setup here
+		done();
+	},
+	'no args': function(test) {
+		arykow_iframely_client('http://h16free.com/2013/12/08/26614-ne-partez-pas-on-est-si-bien-ici', function(err, result) {
+			test.expect(2);
+			test.equal(err, null);
+			test.notEqual(result, null);
+			test.done();
+		});
+	},
 };
